7872ea3bc1deb73e4880a8535fd5cfc992d742dd
3cordguy <jweaver911@gmail.com>
2023-12-06 18:08:33 -0500

    swaps out arrow colors

--- src/components/right-arrow.svelte
+++ src/components/right-arrow.svelte
@@ -4,7 +4,7 @@
 	fill="none"
 	viewBox="0 0 24 24"
 	stroke-width="1.5"
-	stroke="currentColor"
+	stroke="#fff"
 >
 	<path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
 </svg>
